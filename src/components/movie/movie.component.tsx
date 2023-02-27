8b49653a5d533e9bacd642f0ad62a180202aae08
Albany Pineda <agpv.dev@gmail.com>
2023-02-26 19:44:25 -0500

    Dependencies and initial routes added

---
+++ src/components/movie/movie.component.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function MovieComponent() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          this is the movie component, welcome to react routing
+        </div>
+      </header>
+    </div>
+  )
+}
+
+export default MovieComponent
--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import App from './App.tsx'
+import MovieComponent from './components/movie/movie.component.tsx'
+import LoginComponent from './components/auth/login.component.tsx'
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/movies" element={<MovieComponent />} />
+      <Route path="/auth/login" element={<LoginComponent />} />
+      <Route exact path="/" element={<App />}></Route>
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
